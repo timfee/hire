1e1769064a1ef2eaa82206b6d7130b79e924499d
Tim Feeley <tim@timfeeley.com>
2023-01-31 19:18:34 -0800

    Update head.tsx

--- app/[slug]/[code]/head.tsx
+++ app/[slug]/[code]/head.tsx
@@ -55,7 +55,7 @@ export default async function Head({
   return (
     <>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="robots" content="index,follow" />
       <meta name="theme-color" content={companyData.color} />
 
