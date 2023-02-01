9f50199758aed051e2aefa2d2e1f631004b1d2c4
Tim Feeley <tim@timfeeley.com>
2023-01-31 19:43:03 -0800

    fix scrolling issue

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,18 +1,15 @@
 'use client'
 import { Analytics } from '@vercel/analytics/react'
-import { useSearchParams } from 'next/navigation'
 import PlausibleProvider from 'next-plausible'
 
 export function AnalyticsWrapper() {
-  const searchParams = useSearchParams()
   return (
     <>
-      <Analytics />{' '}
+      <Analytics />
       <PlausibleProvider
         domain="hire.timfeeley.com"
         trackFileDownloads
         trackOutboundLinks
-        enabled={searchParams.get('ld') ? false : true}
       />
     </>
   )
