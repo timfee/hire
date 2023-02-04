fe730dcd7989a4ca26809bf13d110a753bf312bb
Tim Feeley <tim@timfeeley.com>
2023-02-04 15:31:02 -0800

    prettier stuff

--- pages/[slug]/[code]/index.tsx
+++ pages/[slug]/[code]/index.tsx
@@ -6,23 +6,30 @@ import type {
   NextPage,
 } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import type { NextSeoProps } from 'next-seo'
 import { NextSeo } from 'next-seo'
+import ReactMarkdown from 'react-markdown'
 
-import Letter from '@/components/Letter/Page'
+import resumeThumbnail from '@/components/Letter/resume_thumbnail.png'
+import { Signature } from '@/components/Letter/Signature'
+import { Stationery } from '@/components/Letter/Stationery'
 import { getLatestResume } from '@/lib/amazon'
 import prisma from '@/lib/prisma'
 
 type PageParams = Pick<Company, 'code' | 'slug'>
 
-const CompanyPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
-  companyData
-) => {
-  const { slug, code } = companyData
-
-  const title = `Tim Feeley ${
-    companyData ? '& ' + companyData.name + ' = ❤️' : ''
-  }`
+const CompanyPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  code,
+  color,
+  resumeUrl,
+  lastUpdated,
+  name,
+  slug,
+  svg,
+  websiteMessage,
+}) => {
+  const title = `Tim Feeley + ${name} = ❤️`
 
   const description =
     'I’m a PM & UX leader with two decades of experience developing high-performing teams and delivering impactful products used by billions of people.'
@@ -30,7 +37,7 @@ const CompanyPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
   const SEO_PARAMS: NextSeoProps = {
     title,
     description,
-    themeColor: companyData.color,
+    themeColor: color,
     useAppDir: true,
     canonical: `https://hire.timfeeley.com/${slug}/${code}`,
     openGraph: {
@@ -61,12 +68,36 @@ const CompanyPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
   return (
     <>
       <Head>
-        <meta name="theme-color" content={companyData.color} />
+        <meta name="theme-color" content={color} />
+        <title>{title}</title>
         <NextSeo {...SEO_PARAMS} />
       </Head>
 
-      <main className="mb-12">
-        <Letter {...companyData} />
+      <main
+        className="relative mx-4 mt-3 max-w-3xl rounded-md border-t-4 bg-white p-4 shadow-md sm:mx-auto sm:mt-12 sm:p-8"
+        style={{ borderTopColor: color }}>
+        <Stationery {...{ svg, name, color, lastUpdated }} />
+        <ReactMarkdown className="prose mt-6 font-serif sm:mt-12">
+          {websiteMessage}
+        </ReactMarkdown>
+        <Signature />
+        {resumeUrl && (
+          <a
+            className="group relative mx-4 mt-8  block max-w-sm  rounded-lg border border-slate-300 bg-slate-100 p-4 text-center sm:mx-auto sm:p-8"
+            download
+            target="blank"
+            href={resumeUrl}
+            rel="noreferrer">
+            <Image
+              src={resumeThumbnail}
+              alt="Resume"
+              height={100}
+              className="mx-auto mb-4 transition-opacity duration-500 ease-in-out"
+            />
+
+            <span className="rounded-full bg-slate-200 py-1 px-4 text-xs text-blue-600 underline transition-all duration-500 ease-in-out group-hover:bg-blue-600 group-hover:text-white sm:text-sm">{`Tim Feeley - ${name}.pdf`}</span>
+          </a>
+        )}
       </main>
     </>
   )
