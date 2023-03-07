d03cdfb059bc8d1eef15f9ee822fd79c888595a1
Tim Feeley <tim@timfeeley.com>
2023-03-06 17:21:44 -0800

    admin

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,12 +1,14 @@
 import aiRouter from './routers/ai'
 import brandRouter from './routers/brand'
 import companyRouter from './routers/company'
+import { humanRouter } from './routers/human'
 import { createTRPCRouter } from './trpc'
 
 export const appRouter = createTRPCRouter({
   ai: aiRouter,
   brand: brandRouter,
   company: companyRouter,
+  human: humanRouter,
 })
 
 export type AppRouter = typeof appRouter
